3463f9f7e0cfad93ff28ee041e64d36e9d64bbee
Keith <Keith.macario@gmail.com>
2022-06-05 19:34:10 -0700

    Removed scored and added i to charImage. Make the project smaller

--- src/Components/CharImage/CharImage.js
+++ src/Components/CharImage/CharImage.js
@@ -1,20 +1,36 @@
 import React, {useState, useEffect } from  "react";
+import Score from "../Score/Score";
 
 // This will create an array of the characters. Whenever the Array is clicked on, it'll store of the index value is true or false.
 
 const CharacterImage = () => {
+    //array of images
     const [charArray, setCharArray] =  useState(["vulkan.jpg"]);
-    const [charClicked, setCharClick] = useState(false);
+    // Checks to see if clicked
+    const [charClicked, setCharClick] = useState([]);
+    // Holds the score
+    const [score, setScore] = useState(0);
+    // Holds the high score
+    const [highScore, setHighScore] = useState(0);
 
-    
+    //
     useEffect(() =>{
-        const charClick = () => {
-            setCharClick(true);
-            console.log("charClicked");
-            return;
-        }
+        //Checks to see if image is clicked.
+        const charClick = (character) => {
+            if(charClicked.includes(character)){
+                setScore(0);
+                setCharClick([]);
+            }
 
-        document.addEventListener("click", charClick);
+            else {
+                //shallow merge charClickedArray
+                setCharClick([...charClick, character])
+                setScore(score + 1);
+                if(score > highScore){
+                    setHighScore(score);
+                }
+            }
+        }
 
     })
     return(
@@ -31,7 +47,7 @@ const CharacterImage = () => {
         }}
         >
              {charArray.map(image=> (
-                 <img src={`images/${image}`} onClick={charClick} />
+                 <img src={`images/${image}`} onClick={charClick(image)} />
              ))}
       </div>
     </div>
