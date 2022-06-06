6b5a57cec6c9097096f96ff305c652e74c10acbc
Keith <Keith.macario@gmail.com>
2022-06-05 18:21:37 -0700

    Fixed the issue with the image not loading originally. It has since been added to the array directly.

--- src/Components/CharImage/CharImage.js
+++ src/Components/CharImage/CharImage.js
@@ -3,13 +3,14 @@ import React, {useState, useEffect } from  "react";
 // This will create an array of the characters. Whenever the Array is clicked on, it'll store of the index value is true or false.
 
 const CharacterImage = () => {
-    const [charArray, setCharArray] =  useState([]);
+    const [charArray, setCharArray] =  useState(["vulkan.jpg"]);
     const [charClicked, setCharClick] = useState(false);
 
     
     useEffect(() =>{
         const charClick = () => {
             setCharClick(true);
+            console.log("charClicked");
             return;
         }
 
@@ -29,7 +30,9 @@ const CharacterImage = () => {
             top: "50%",
         }}
         >
-             <img src="vulkan.jpg" alt="Nothing" width="50px" height="50px"> </img>
+             {charArray.map(image=> (
+                 <img src={`images/${image}`} onClick={charClick} />
+             ))}
       </div>
     </div>
     )
