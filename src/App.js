d7b06264edb576db9a76a14cf16a173ae5ac0314
Keith <Keith.macario@gmail.com>
2022-06-09 20:59:28 -0700

    Added a shufler function. Working on useEffect error.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import CharacterImage from './Components/CharImage/CharImage';
-import Score from './Components/Score/Score';
 
 function App() {
   return (
     <div className="App">
-      <Score />
       <CharacterImage />
     </div>
   );
--- src/Components/CharImage/CharImage.js
+++ src/Components/CharImage/CharImage.js
@@ -1,7 +1,20 @@
-import React, {useState, useEffect } from  "react";
-import Score from "../Score/Score";
+/* This program tests a player/user's memory by clicking an image. 
+The image order changes and the score is adjusted each time the player
+clicks an image they haven't clicked yet.
+*/
 
-// This will create an array of the characters. Whenever the Array is clicked on, it'll store of the index value is true or false.
+import React, {useState, useEffect } from  "react";
+
+// This will create an array of the characters. Whenever the Array is clicked on, it'll store of the index value is true or false
+
+
+const shuffle = (array) => {
+    for (let i = array.length - 1; i > 0; i--){
+        const j = Math.floor.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+}
 
 const CharacterImage = () => {
     //array of images
@@ -13,24 +26,35 @@ const CharacterImage = () => {
     // Holds the high score
     const [highScore, setHighScore] = useState(0);
 
-    //
+    
     useEffect(() =>{
         //Checks to see if image is clicked.
         const charClick = (character) => {
             if(charClicked.includes(character)){
+                //if it is clicked, reset score.
                 setScore(0);
+                //clears the array which hold checked image.
                 setCharClick([]);
             }
 
             else {
                 //shallow merge charClickedArray
+                
                 setCharClick([...charClick, character])
+                
                 setScore(score + 1);
+
                 if(score > highScore){
+
                     setHighScore(score);
+
                 }
             }
+            //shuffles the images every click.
+            setCharArray(shuffle(charArray));
         }
+
+        setCharArray(shuffle(charArray));
 
     })
     return(
@@ -46,9 +70,15 @@ const CharacterImage = () => {
             top: "50%",
         }}
         >
-             {charArray.map(image=> (
-                 <img src={`images/${image}`} onClick={charClick(image)} />
-             ))}
+            <div className="charContainer">
+                {
+                    charArray.map((image)=> (
+                        <div className="char" onClick={()=> charClick(image)}>
+                            <img alt={`primarch-${image}`} src={`${image}.jpg`} />
+                    </div>
+                    ))
+                    }
+                    </div>
       </div>
     </div>
     )
